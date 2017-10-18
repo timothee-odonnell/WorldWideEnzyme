47bda0a72c2580df6a073233b8bac2beda83f2e0
htyao <anthonyhtyao@gmail.com>
2017-10-19 00:12:26 +0200

    Fix bug

--- app/templatetags/app_tags.py
+++ app/templatetags/app_tags.py
@@ -1,10 +1,10 @@
 from django import template
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 import re
 
 register = template.Library()
 
 @register.filter(name='ec')
 def ec(value):
-    url_ec = str(reverse('app.views.enzyme_page',args=['']))
+    url_ec = str(reverse('enzyme_page',args=['']))
     return re.sub(r'EC (\d\.\d{1,2}\.\d{1,2}\.\w{1,3})',r'<a href="{0}\1">EC \1</a>'.format(url_ec),value)
