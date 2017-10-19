67ffed54fe33de066cb0e1fcafc5d793b3a28a55
htyao <anthonyhtyao@gmail.com>
2017-10-19 16:38:39 +0200

    Change url of enzyme api

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api import views
 
 urlpatterns = [
     url(r'^enzymes/$', views.EnzymeList.as_view()),
-    url(r'^enzymes/(?P<label>.*)$', views.EnzymeDetail.as_view()),
+    url(r'^enzymes/(?P<label>\d\.\d{1,2}\.\d{1,2}\.\w{1,3})$', views.EnzymeDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
