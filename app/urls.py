821def7b2245ec4669ca56c7f64a0fb286734dd8
htyao <anthonyhtyao@gmail.com>
2017-10-19 16:31:10 +0200

    Simple pages for cofactors

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from app import views
 urlpatterns = [
     url(r'^$',views.main, name='main'),       
     url(r'^enzymes/(?P<label>\d\.\d{1,2}\.\d{1,2}\.\w{1,3})$',views.enzyme_page, name='enzyme_page'),       
+    url(r'^cofactors$',views.cofactors_list, name='cofactors_list'),       
+    url(r'^cofactors/(?P<cofactor>.*)$',views.cofactors_page, name='cofactors_page'),       
 ]
