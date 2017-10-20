c7e1b06c35f4bb396175731228f4ffd4ccdd7727
htyao <anthonyhtyao@gmail.com>
2017-10-20 02:33:14 +0200

    Hyperlink between enzyme and article

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
 urlpatterns = [
-    url(r'^enzymes/$', views.EnzymeList.as_view()),
-    url(r'^enzymes/(?P<label>\d\.\d{1,2}\.\d{1,2}\.\w{1,3})$', views.EnzymeDetail.as_view()),
-    url(r'^articles/$', views.ArticleList.as_view()),
-    url(r'^articles/(?P<pk>[0-9]+)$', views.ArticleDetail.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^enzymes/$', views.EnzymeList.as_view(),name='enzyme-list'),
+    url(r'^enzymes/(?P<label>\d\.\d{1,2}\.\d{1,2}\.\w{1,3})$', views.EnzymeDetail.as_view(),name='enzyme-detail'),
+    url(r'^articles/$', views.ArticleList.as_view(),name='article-list'),
+    url(r'^articles/(?P<pk>[0-9]+)$', views.ArticleDetail.as_view(),name='article-detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,16 @@
 from rest_framework import mixins, generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from app.models import Enzyme, Article
 from api.serializers import EnzymeSerializer, ArticleSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'enzymes': reverse('enzyme-list', request=request, format=format),
+        'articles': reverse('article-list', request=request, format=format)
+    })
 
 class EnzymeList(generics.ListCreateAPIView):
     queryset = Enzyme.objects.all()
